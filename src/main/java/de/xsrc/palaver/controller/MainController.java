a8e7074ea3d28f2c69ddbb1e0c784836ee31013c
Bahtiar Gadimov <bahtiar@gadimov.de>
2014-08-19 18:44:26 +0200

    MainController now populates palaversList

--- src/main/java/de/xsrc/palaver/controller/MainController.java
+++ src/main/java/de/xsrc/palaver/controller/MainController.java
@@ -1,12 +1,16 @@
 package de.xsrc.palaver.controller;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 
 import org.datafx.controller.FXMLController;
 import org.datafx.controller.flow.Flow;
 import org.datafx.controller.flow.FlowException;
 import org.datafx.controller.flow.action.LinkAction;
+
+import de.xsrc.palaver.model.Palaver;
 
 @FXMLController("/fxml/MainView.fxml")
 public class MainController {
@@ -14,6 +18,15 @@ public class MainController {
 	@FXML
 	@LinkAction(AccountController.class)
 	private Button showAccountsButton;
+
+	@FXML
+	private ListView<Palaver> palaverList;
+
+	@FXML
+	private void initialize() {
+		ObservableList<Palaver> palavers = Utils.getStorage(Palaver.class).getAll();
+		palaverList.setItems(palavers);
+	}
 
 	@FXML
 	private void addPalaver() {
