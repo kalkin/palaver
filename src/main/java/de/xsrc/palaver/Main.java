151731aa69c4e2deb56297280d812abe9c126f04
Bahtiar Gadimov <bahtiar@gadimov.de>
2014-08-20 00:34:24 +0200

    Added smack

--- src/main/java/de/xsrc/palaver/Main.java
+++ src/main/java/de/xsrc/palaver/Main.java
@@ -1,8 +1,13 @@
 package de.xsrc.palaver;
 
+import java.io.IOException;
 import java.util.ResourceBundle;
+import java.util.logging.Logger;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.ObservableMap;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,10 +15,20 @@ import org.datafx.controller.flow.Flow;
 import org.datafx.controller.flow.FlowException;
 import org.datafx.controller.flow.FlowHandler;
 import org.datafx.controller.flow.container.DefaultFlowContainer;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 
 import de.xsrc.palaver.controller.MainController;
+import de.xsrc.palaver.model.Account;
+import de.xsrc.palaver.utils.Storage;
+import de.xsrc.palaver.utils.Utils;
 
 public class Main extends Application {
+
+	private static final Logger logger = Logger.getLogger(Storage.class
+			.getName());
 
 	@Override
 	public void start(Stage primaryStage) throws FlowException {
@@ -21,6 +36,29 @@ public class Main extends Application {
 		Scene scene = handleI18n(flow);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		handleXmpp();
+	}
+
+	private ObservableMap<Account,XMPPConnection> handleXmpp() {
+		ObservableList<Account> accountList = Utils.getStorage(Account.class)
+				.getAll();
+		ObservableMap<Account, XMPPConnection> conMap = FXCollections
+				.observableHashMap();
+		for (Account account : accountList) {
+			logger.fine("Connection account " + account);
+			String keys[] = account.getJid().split("@");
+			XMPPConnection c = new XMPPTCPConnection(keys[1]);
+			try {
+				c.connect();
+				c.login(keys[0], account.getPassword());
+				logger.info("Connected account: " + account);
+				conMap.put(account, c);
+			} catch (SmackException | IOException | XMPPException e) {
+				logger.warning("Can not connect to server: " + keys[1]);
+				e.printStackTrace();
+			}
+		}
+		return conMap;
 	}
 
 	private Scene handleI18n(Flow f) throws FlowException {
