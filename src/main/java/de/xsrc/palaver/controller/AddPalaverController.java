dc08fd819d8bcdc89129ffda80fb512f7a25d69a
Bahtiar Gadimov <bahtiar@gadimov.de>
2014-08-19 14:26:22 +0200

    A:AddPalaverController added action addPalaverAction

--- src/main/java/de/xsrc/palaver/controller/AddPalaverController.java
+++ src/main/java/de/xsrc/palaver/controller/AddPalaverController.java
@@ -2,15 +2,32 @@ package de.xsrc.palaver.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import org.datafx.controller.FXMLController;
+import org.datafx.crud.CrudException;
+
+import de.xsrc.palaver.model.Palaver;
 
 @FXMLController("/fxml/AddPalaverView.fxml")
 public class AddPalaverController {
 
 	@FXML
 	private Button back;
+
+	@FXML
+	private TextField jid;
+
+	@FXML
+	private void addPalaverAction() throws CrudException {
+		Palaver p = new Palaver();
+		p.setRecipient(jid.getText());
+		p.setAccount("adolf@xsrc.de");
+		Utils.getStorage(Palaver.class).getAll().add(p);
+		Stage stage = (Stage) back.getScene().getWindow();
+		stage.close();
+	}
 
 	@FXML
 	private void close() {
