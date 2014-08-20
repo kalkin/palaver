cb3c1c608c7199e30160620068f50b4c677350b2
Bahtiar Gadimov <bahtiar@gadimov.de>
2014-08-20 05:35:39 +0200

    Added large icon to eventview

--- src/main/java/de/xsrc/palaver/controller/EntryController.java
+++ src/main/java/de/xsrc/palaver/controller/EntryController.java
@@ -1,7 +1,10 @@
 package de.xsrc.palaver.controller;
 
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 
 import org.datafx.controller.FXMLController;
 
@@ -15,10 +18,13 @@ public class EntryController {
 	@FXML
 	private Label body;
 
+	@FXML
+	private Label from;
+
 	public void setEntry(Entry e) {
 		this.entry = e;
 		body.textProperty().bind(entry.bodyProperty());
-
+		from.setText(e.getFrom().substring(0, 1).toUpperCase());
 	}
 
 	@FXML
