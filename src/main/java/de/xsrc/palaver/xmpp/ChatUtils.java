f31d30ce738f122917d2769c576feeae906ad2c0
Bahtiar Gadimov <bahtiar@gadimov.de>
2014-08-20 14:26:59 +0200

    A:TrustManager which trusts all CAs

--- src/main/java/de/xsrc/palaver/xmpp/ChatUtils.java
+++ src/main/java/de/xsrc/palaver/xmpp/ChatUtils.java
@@ -4,8 +4,13 @@ import java.io.IOException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ChatManager;
+import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.SmackException.NotConnectedException;
 import org.jivesoftware.smack.XMPPConnection;
@@ -42,13 +47,30 @@ public class ChatUtils {
 			throws SmackException, IOException, XMPPException {
 		logger.finer("Connecting to account " + a);
 		String jid = a.getJid();
-		XMPPConnection c = new XMPPTCPConnection(StringUtils.parseServer(jid));
+
+		ConnectionConfiguration config = configureConnection(StringUtils
+				.parseServer(jid));
+
+		XMPPConnection c = new XMPPTCPConnection(config);
 		c.connect();
 		c.login(StringUtils.parseName(jid), a.getPassword());
 		ChatManager.getInstanceFor(c).addChatListener(new ChatListener(a));
 		getConMap().put(a.getJid(), c);
 
 		return c;
+	}
+
+	private static ConnectionConfiguration configureConnection(String server) {
+		ConnectionConfiguration config = new ConnectionConfiguration(server);
+		SSLContext context;
+		try {
+			context = getContext();
+			config.setCustomSSLContext(context);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return config;
 	}
 
 	private synchronized static Chat getChat(Palaver palaver) {
@@ -74,5 +96,39 @@ public class ChatUtils {
 		} catch (NotConnectedException | XMPPException e1) {
 			e1.printStackTrace();
 		}
+	}
+
+	protected static SSLContext getContext() throws Exception {
+		try {
+			SSLContext sslContext = SSLContext.getInstance("SSL");
+			sslContext.init(null, getTrustManager(),
+					new java.security.SecureRandom());
+
+			return sslContext;
+		} catch (Exception e) {
+			throw new Exception("Error creating context for SSLSocket!", e);
+		}
+	}
+
+	/**
+	 * Create a trust manager that trusts all certificates It is not using a
+	 * particular keyStore
+	 */
+	protected static TrustManager[] getTrustManager() {
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+			public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(
+					java.security.cert.X509Certificate[] certs, String authType) {
+			}
+
+			public void checkServerTrusted(
+					java.security.cert.X509Certificate[] certs, String authType) {
+			}
+		} };
+
+		return trustAllCerts;
 	}
 }
