11b1bbd8f5323c9646b21302df30578228896cd0
Bahtiar Gadimov <bahtiar@gadimov.de>
2014-08-21 02:53:53 +0200

    F:Avatar lists

--- src/main/java/de/xsrc/palaver/controller/PalaverCell.java
+++ src/main/java/de/xsrc/palaver/controller/PalaverCell.java
@@ -1,7 +1,11 @@
 package de.xsrc.palaver.controller;
 
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Text;
 
 import org.jivesoftware.smack.util.StringUtils;
 
@@ -12,15 +16,26 @@ public class PalaverCell extends ListCell<Palaver> {
 	public void updateItem(Palaver p, boolean empty) {
 		super.updateItem(p, empty);
 		if (p != null) {
-			Label l = new Label();
-			l.setText(p.getRecipient().substring(0, 1));
 			String name = StringUtils.parseName(p.getRecipient());
-			int modena_colors[] = {0xf3622d, 0xfba71b, 0xFF673ab7, 0x41a9c9, 0x9a42c8, 0xc84164, 0xFF00bcd4, 0x888888};
-			int color = modena_colors[(int) ((name.hashCode() & 0xffffffffl) % modena_colors.length)];
-			String hexcolor =  String.format("#%06X", (0xFFFFFF & color));
-			l.setStyle("-fx-background-color:" + hexcolor);
-			setGraphic(l);
-			setText(p.getRecipient());
+			if (name != null && name.length() > 0) {
+				int modena_colors[] = { 0xf3622d, 0xfba71b, 0xFF673ab7,
+						0x41a9c9, 0x9a42c8, 0xc84164, 0xFF00bcd4, 0x888888 };
+				int color = modena_colors[(int) ((name.hashCode() & 0xffffffffl) % modena_colors.length)];
+				String hexcolor = String.format("#%06X", (0xFFFFFF & color));
+				Rectangle r = new Rectangle(64, 64);
+				r.setFill(Paint.valueOf(hexcolor));
+				r.setArcHeight(5);
+				r.setArcWidth(5);
+				r.setStrokeType(StrokeType.INSIDE);
+				r.setStroke(Paint.valueOf("BLACK"));
+				StackPane sp = new StackPane();
+				Text t = new Text(name.substring(0, 1).toUpperCase());
+				t.setFont(getFont().font(60));
+				sp.getChildren().add(r);
+				sp.getChildren().add(t);
+				setGraphic(sp);
+				setText(p.getRecipient());
+			}
 		}
 	}
 }
