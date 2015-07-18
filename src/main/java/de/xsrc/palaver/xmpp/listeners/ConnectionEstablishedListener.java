5dfe92a7b70593473f6f51d496b52696e7bac2b4
Bahtiar `kalkin-` Gadimov <kalkin@xsrc.de>
2015-07-18 15:36:47 +0200

    Replace PalaverConnectionListener with ConnectionEstablishedListener

--- src/main/java/de/xsrc/palaver/xmpp/listeners/ConnectionEstablishedListener.java
+++ src/main/java/de/xsrc/palaver/xmpp/listeners/ConnectionEstablishedListener.java
@@ -1,5 +1,6 @@
 package de.xsrc.palaver.xmpp.listeners;
 
+import de.xsrc.palaver.Connection;
 import de.xsrc.palaver.beans.Credentials;
 import de.xsrc.palaver.models.ContactModel;
 import de.xsrc.palaver.utils.Utils;
@@ -8,14 +9,14 @@ import javafx.collections.MapChangeListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.roster.Roster;
 import org.jivesoftware.smack.roster.rosterstore.DirectoryRosterStore;
-import org.jivesoftware.smack.tcp.XMPPTCPConnection;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Bahtiar `kalkin-` Gadimov on 29.06.15.
  */
-public class ConnectionEstablishedListener implements MapChangeListener<Credentials, XMPPTCPConnection> {
+public class ConnectionEstablishedListener implements MapChangeListener<Credentials, Connection> {
 
     private static final String WORKING_DIRECTORY = Utils.getConfigDirectory() + "/roster/";
     private final ContactModel contactModel;
@@ -24,50 +25,49 @@ public class ConnectionEstablishedListener implements MapChangeListener<Credenti
         this.contactModel = contactModel;
     }
 
-    @Override
-    public void onChanged(MapChangeListener.Change<? extends Credentials, ? extends XMPPTCPConnection> c) {
-        if (c.wasAdded()) {
-            final XMPPConnection connection = c.getValueAdded();
-            final Credentials credentials = c.getKey();
-            final Roster roster = Roster.getInstanceFor(connection);
-            final String jid = credentials.getJid();
-            setupRosterStore(WORKING_DIRECTORY + jid, roster);
-            setupRosterEntriesSynchronisation(credentials, roster, contactModel);
-
-        }
-    }
-
     /**
      * Initializes or opens a {@link DirectoryRosterStore} for storing roster entries in a set of files as specified by
      * RFC 6121 and setups the given {@link Roster} to use it.
      *
      * @param path path to directory where data is/should be stored
-     * @return
      */
-    public static Roster setupRosterStore(String path, Roster roster) {
+    private static void setupRosterStore(String path, Roster roster) throws IOException {
         final File dir = new File(path);
         final DirectoryRosterStore rosterStore;
         if (!dir.exists()) {
-            dir.mkdirs();
-            rosterStore =  DirectoryRosterStore.init(dir);
+            if (dir.mkdirs()) {
+                rosterStore = DirectoryRosterStore.init(dir);
+            } else {
+                throw new IOException("Could not create roster directory");
+            }
         } else {
             rosterStore = DirectoryRosterStore.open(dir);
         }
         roster.setRosterStore(rosterStore);
-        return roster;
     }
 
-    /**
-     *
-     * @param credentials
-     * @param roster
-     * @param contactModel
-     */
     public static void setupRosterEntriesSynchronisation(Credentials credentials, Roster roster, ContactModel contactModel) {
         final RosterEntriesImporter rosterEntriesImporter = new RosterEntriesImporter(credentials, contactModel);
         final PalaverRosterListener palaverRosterListener = new PalaverRosterListener(credentials, contactModel, roster);
         contactModel.registerRoster(credentials, roster);
         roster.getEntriesAndAddListener(palaverRosterListener, rosterEntriesImporter);
+    }
+
+    @Override
+    public void onChanged(MapChangeListener.Change<? extends Credentials, ? extends Connection> c) {
+        if (c.wasAdded()) {
+            final XMPPConnection connection = c.getValueAdded().xmpptcpConnection;
+            final Credentials credentials = c.getKey();
+            final Roster roster = Roster.getInstanceFor(connection);
+            final String jid = credentials.getJid();
+            try {
+                setupRosterStore(WORKING_DIRECTORY + jid, roster);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            setupRosterEntriesSynchronisation(credentials, roster, contactModel);
+
+        }
     }
 
 }
