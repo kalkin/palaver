4129b166609518217884630be747107c379d86d0
Bahtiar `kalkin-` Gadimov <kalkin@xsrc.de>
2015-07-18 12:48:49 +0200

    Simplified AccountProvider

--- src/main/java/de/xsrc/palaver/provider/AccountProvider.java
+++ src/main/java/de/xsrc/palaver/provider/AccountProvider.java
@@ -3,7 +3,6 @@ package de.xsrc.palaver.provider;
 import de.xsrc.palaver.beans.Credentials;
 import de.xsrc.palaver.utils.AppDataSource;
 import javafx.collections.ObservableList;
-import org.datafx.controller.context.ApplicationContext;
 import org.datafx.provider.ListDataProvider;
 import org.datafx.reader.WritableDataReader;
 import org.datafx.writer.WriteBackHandler;
@@ -17,29 +16,16 @@ public class AccountProvider extends ListDataProvider<Credentials> {
         this.setAddEntryHandler(new WriteBackHandler<Credentials>() {
             @Override
             public WritableDataReader<Credentials> createDataSource(Credentials observable) {
-                ObservableList<Credentials> list = ApplicationContext.getInstance()
-                        .getRegisteredObject(AccountProvider.class).getData();
+                ObservableList<Credentials> list = getData();
                 return new AppDataWriter<>(list, Credentials.class);
             }
         });
         this.setWriteBackHandler(new WriteBackHandler<Credentials>() {
             @Override
             public WritableDataReader<Credentials> createDataSource(Credentials observable) {
-                ObservableList<Credentials> list = ApplicationContext.getInstance()
-                        .getRegisteredObject(AccountProvider.class).getData();
+                ObservableList<Credentials> list = getData();
                 return new AppDataWriter<>(list, Credentials.class);
             }
         });
     }
-
-    public static Credentials getByJid(String jid) {
-        ObservableList<Credentials> credentialses = ApplicationContext.getInstance()
-                .getRegisteredObject(AccountProvider.class).getData().get();
-        for (Credentials credentials : credentialses) {
-            if (credentials.getJid().equals(jid))
-                return credentials;
-        }
-        return null;
-    }
-
 }
