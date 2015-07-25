fd46f11d8771f7e0cab2fd454cadc36f16564eb9
Bahtiar `kalkin-` Gadimov <kalkin@xsrc.de>
2015-07-25 11:42:03 +0200

    ContactModel uses now a HashSet for data

--- src/main/java/de/xsrc/palaver/models/ContactModel.java
+++ src/main/java/de/xsrc/palaver/models/ContactModel.java
@@ -5,14 +5,16 @@ import de.xsrc.palaver.beans.Credentials;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.ObservableMap;
+import javafx.collections.ObservableSet;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.roster.Roster;
 import org.jivesoftware.smack.roster.RosterEntry;
 import org.jxmpp.util.XmppStringUtils;
 
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Logger;
 
 // Crosspoint between view, smack and ContactProvider (file backend)
@@ -20,13 +22,14 @@ public class ContactModel {
     private static final Logger logger = Logger.getLogger(ContactModel.class
             .getName());
 
-    private final ObservableList<Contact> data = FXCollections.observableList(new CopyOnWriteArrayList<>());
+    private final ObservableSet<Contact> data = FXCollections.observableSet(new HashSet<>());
 
     private final ObservableMap<String, Roster> rosterMap = FXCollections.observableMap(new
             ConcurrentHashMap<>());
 
     public ObservableList<Contact> getData() {
-        return FXCollections.unmodifiableObservableList(data);
+        final LinkedList<Contact> contactLinkedList = new LinkedList<>(data);
+        return FXCollections.observableList(contactLinkedList);
     }
 
 
