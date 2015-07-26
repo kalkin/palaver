ddd27ad6bd86c9834b33318590a81632184486cc
Bahtiar `kalkin-` Gadimov <kalkin@xsrc.de>
2015-07-26 12:13:51 +0200

    First draft of reworked MucManager

--- src/main/java/de/xsrc/palaver/xmpp/MucManager.java
+++ src/main/java/de/xsrc/palaver/xmpp/MucManager.java
@@ -1,27 +1,95 @@
 package de.xsrc.palaver.xmpp;
 
-/**
- * TODO Rework as normal Object and not a Singleton
- */
+import de.xsrc.palaver.Connection;
+import de.xsrc.palaver.beans.Conversation;
+import de.xsrc.palaver.beans.HistoryEntry;
+import de.xsrc.palaver.models.ConversationManager;
+import de.xsrc.palaver.xmpp.task.JoinMucTask;
+import javafx.application.Platform;
+import javafx.collections.*;
+import org.datafx.concurrent.ObservableExecutor;
+import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.tcp.XMPPTCPConnection;
+import org.jivesoftware.smackx.muc.MultiUserChat;
+
+import java.util.List;
+import java.util.WeakHashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
+
 public class MucManager {
 
-//	private static final Logger logger = Logger
-//					.getLogger(MucManager.class.getName());
-//
-//	private final ObservableList<Palaver> openPalavers;
-//	private ConcurrentHashMap<Palaver, MultiUserChat> joinedMucMap;
-//
-//	private MucManager() {
-//		joinedMucMap = new ConcurrentHashMap<>();
-//
-//		openPalavers = PalaverModel.getInstance().getOpenPalavers();
-//		openPalavers.addListener((ListChangeListener<Palaver>) change -> {
-//			while (change.next()) {
-//				if (change.wasAdded()) {
-//					joinMucs(change.getAddedSubList());
-//				} else if (change.wasRemoved()) {
-//					ObservableExecutor executor = ApplicationContext.getInstance().getRegisteredObject(ObservableExecutor.class);
-//					change.getRemoved().parallelStream().filter(palaver -> palaver.isConference()).forEach(palaver -> {
+    private static final Logger logger = Logger.getLogger(MucManager.class.getName());
+
+    private final ObservableMap<Conversation, MultiUserChat> joinedMucMap = FXCollections.observableMap(new
+            ConcurrentHashMap<>());
+    private final ObservableExecutor executor;
+    private final WeakHashMap<String, XMPPTCPConnection> connectionMap = new WeakHashMap<>();
+    private ConversationManager conversationManager;
+
+    public MucManager(ConversationManager conversationManager, ObservableExecutor executor) {
+        this.conversationManager = conversationManager;
+        this.executor = executor;
+        conversationManager.getOpenConversations().addListener((ListChangeListener<Conversation>) change -> {
+                    while (change.next()) {
+                        if (change.wasAdded()) {
+                            final List<? extends Conversation> conversations = change.getAddedSubList();
+                            conversations.parallelStream().filter(this::isJoinableConference).forEach(this::joinMuc);
+                        } else if (change.wasRemoved()) {
+                            final List<? extends Conversation> removedConversations = change.getRemoved();
+                            removedConversations.parallelStream().filter(Conversation::isConference).forEach(this::leaveMuc);
+                        }
+                    }
+                }
+
+        );
+        joinedMucMap.addListener((MapChangeListener<Conversation, MultiUserChat>) change -> {
+            if (change.wasAdded()) {
+                final MultiUserChat muc = change.getValueAdded();
+                final Conversation conversation = change.getKey();
+                conversation.history.entryListProperty().addListener((ListChangeListener<HistoryEntry>) c -> sendNewMsgs(conversation, c));
+            }
+        });
+
+    }
+
+    public void sendNewMsgs(Conversation conversation, ListChangeListener.Change<? extends HistoryEntry> c) {
+        while (c.next()) if (c.wasAdded()) {
+            final MultiUserChat multiUserChat = joinedMucMap.get(conversation);
+            c.getAddedSubList().stream().filter(historyEntry -> !historyEntry.getSendState())
+                    .forEach(historyEntry -> {
+                        try {
+                            logger.severe(historyEntry.getBody());
+                            multiUserChat.sendMessage(historyEntry.getBody());
+                            historyEntry.setSendState(true);
+                        } catch (SmackException.NotConnectedException e) {
+                            e.printStackTrace();
+                            logger.severe("Could not send muc msg to " + conversation);
+                        }
+                    });
+        }
+    }
+
+    /**
+     * Return true if conversation is conference and we have an {@link XMPPTCPConnection} in {@link
+     * MucManager#connectionMap} for it.
+     *
+     * @param conversation
+     * @return
+     */
+    protected boolean isJoinableConference(Conversation conversation) {
+        return conversation.isConference() && connectionMap.containsKey(conversation.getAccount());
+    }
+
+    protected void joinMuc(Conversation conversation) {
+        logger.severe("Joining muc " + conversation);
+        JoinMucTask task = new JoinMucTask(conversation, connectionMap.get(conversation.getAccount()));
+        Platform.runLater(() -> task.setOnSucceeded(event -> joinedMucMap.put(conversation, (MultiUserChat) task.getValue())));
+        executor.submit(task);
+    }
+
+    protected void leaveMuc(Conversation conversation) {
 //						executor.submit(() -> {
 //							try {
 //								BookmarkManager bm = BookmarkManager.getBookmarkManager(ConnectionManager.getConnection(palaver.getAccount()));
@@ -36,50 +104,18 @@ public class MucManager {
 //								logger.warning(String.format("Could not get Bookmark manager on %s", palaver.toString()));
 //							}
 //						});
-//					});
-//				}
-//			}
-//		});
-//	}
-//
-//	public static MucManager getInstance() {
-//		return InstanceHolder.INSTANCE;
-//	}
-//
-//	private void joinMucs(List<? extends Palaver> list) {
-//		ObservableExecutor executor = ApplicationContext.getInstance().getRegisteredObject(ObservableExecutor.class);
-//		list.forEach(palaver -> {
-//							if (palaver.isConference()) {
-//								JoinMucTask task = new JoinMucTask(palaver);
-//								Platform.runLater(() -> task.setOnSucceeded(event -> joinedMucMap.put(palaver, (MultiUserChat) task.getValue())));
-//
-//								executor.submit(task);
-//							}
-//						}
-//		);
-//	}
-//
-//	public void disconnect(String accountJid) {
-//		joinedMucMap.forEach((palaver, multiUserChat) -> {
-//			if (palaver.getAccount().equals(accountJid)) {
-//				joinedMucMap.remove(palaver);
-//			}
-//		});
-//	}
-//
-//	public void reconnect(String accountJid) {
-//
-//		List<Palaver> mucsToRejoin = openPalavers.parallelStream().filter(palaver -> palaver.isConference() && palaver.getAccount().equals(accountJid)).collect(Collectors.toList());
-//		logger.info(String.format("Rejoining the mucs %s", mucsToRejoin.toString()));
-//		joinMucs(mucsToRejoin);
-//	}
-//
-//	public MultiUserChat getMuc(Palaver palaver) {
-//		return joinedMucMap.get(palaver);
-//	}
-//
-//	private static final class InstanceHolder {
-//		static final MucManager INSTANCE = new MucManager();
-//	}
+//                });
+
+    }
+
+
+    public void registerConnection(Connection connection) {
+        final String accountJid = connection.getCredentials().getJid();
+        logger.finer("Register connection " + accountJid);
+        connectionMap.put(accountJid, connection.xmpptcpConnection);
+        final ObservableList<Conversation> openConversations = conversationManager.getOpenConversations();
+        openConversations.parallelStream().filter(Conversation::isConference)
+                .filter(conversation -> conversation.getAccount().equals(accountJid)).forEach(this::joinMuc);
+    }
 
 }
