4a9ea56d163ce6a3995917fbdde1c8e4ed51f9a4
Bahtiar `kalkin-` Gadimov <kalkin@xsrc.de>
2015-07-26 12:12:40 +0200

    Fix MucListener

--- src/main/java/de/xsrc/palaver/xmpp/listeners/MucListener.java
+++ src/main/java/de/xsrc/palaver/xmpp/listeners/MucListener.java
@@ -1,44 +1,48 @@
 package de.xsrc.palaver.xmpp.listeners;
 
 
-import de.xsrc.palaver.beans.HistoryEntry;
 import de.xsrc.palaver.beans.Conversation;
+import de.xsrc.palaver.beans.HistoryEntry;
 import de.xsrc.palaver.utils.Notifications;
-import org.jivesoftware.smack.PacketListener;
-import org.jivesoftware.smack.SmackException;
+import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.Stanza;
 import org.jxmpp.util.XmppStringUtils;
 
 import java.util.logging.Logger;
 
-public class MucListener implements PacketListener {
+public class MucListener implements MessageListener {
 
-    private static final Logger logger = Logger
-            .getLogger(MucListener.class.getName());
+    private static final Logger logger = Logger.getLogger(MucListener.class.getName());
     private Conversation conversation;
 
     public MucListener(Conversation conversation) {
         this.conversation = conversation;
-        logger.fine("Create MucListener for " + conversation);
+        logger.finer("Create MucListener for " + conversation);
     }
 
+
     @Override
-    public void processPacket(Stanza packet) throws SmackException.NotConnectedException {
-        if (packet instanceof Message) {
-            Message message = (Message) packet;
-            logger.finer("Received muc message");
-            String body = message.getBody();
-            logger.finest(message.toString());
+    public void processMessage(Message message) {
+        logger.finer("Received muc message for " + conversation);
+        final String body = message.getBody();
+        logger.finest(message.toString());
+        final String chatUsername = XmppStringUtils.parseResource(message.getFrom());
+        final String myUsername = XmppStringUtils.parseLocalpart(conversation.getAccount());
 
-            if (message.getType() == Message.Type.groupchat && body != null && message.getBody().length() >= 0) {
-                HistoryEntry historyEntry = new HistoryEntry(XmppStringUtils.parseResource(message.getFrom()), message.getBody());
-                conversation.history.addEntry(historyEntry);
-                if (!message.getFrom().equals(XmppStringUtils.parseLocalpart(conversation.getAccount())))
-                    conversation.setUnread(true);
-                Notifications.notify(XmppStringUtils.parseLocalpart(message.getFrom()), message.getBody());
-
-            }
+        if (chatUsername.equals(myUsername)) {
+            // Our own message, this can be ignored
+            return;
         }
+
+        if (message.getType() == Message.Type.groupchat && body != null && body.length() >= 0) {
+            HistoryEntry historyEntry = new HistoryEntry(chatUsername, body);
+            conversation.setUnread(true);
+            historyEntry.setSendState(true);
+            Notifications.notify(XmppStringUtils.parseLocalpart(message.getFrom()), message.getBody());
+            conversation.add(historyEntry);
+        } else {
+            logger.warning("Strange muc message" + message.toString());
+        }
+
     }
 }
